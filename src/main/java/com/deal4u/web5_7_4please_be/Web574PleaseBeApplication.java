2965b5108fc943dbee142705dc0baea444a397a6
ZELDA31777 <3177link@gmail.com>
2025-07-08 15:22:25 +0900

    ci: Initial Spring Boot project And CI setup

---
+++ src/main/java/com/deal4u/web5_7_4please_be/Web574PleaseBeApplication.java
@@ -0,0 +1,13 @@
+package com.deal4u.web5_7_4please_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Web574PleaseBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Web574PleaseBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/deal4u/web5_7_4please_be/Web574PleaseBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deal4u.web5_7_4please_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Web574PleaseBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
