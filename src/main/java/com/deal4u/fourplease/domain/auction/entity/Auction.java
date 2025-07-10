b2fbf3a378001697aeb8fbcad270e08b420d3d43
pbk2312 <pbk2312@inu.ac.kr>
2025-07-11 00:20:06 +0900

    feat(order): 빌더 및 생성자 추가

--- src/main/java/com/deal4u/fourplease/domain/auction/entity/Auction.java
+++ src/main/java/com/deal4u/fourplease/domain/auction/entity/Auction.java
@@ -12,9 +12,15 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToOne;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Auction extends BaseDateEntity {
 
