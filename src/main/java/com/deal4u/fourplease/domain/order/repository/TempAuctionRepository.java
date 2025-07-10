d5615be85a35bc794eb74023debb23018a0e88c8
pbk2312 <pbk2312@inu.ac.kr>
2025-07-11 00:19:33 +0900

    feat(order): Temp 레포지토리 작성

---
+++ src/main/java/com/deal4u/fourplease/domain/order/repository/TempAuctionRepository.java
@@ -0,0 +1,13 @@
+package com.deal4u.fourplease.domain.order.repository;
+
+import com.deal4u.fourplease.domain.auction.entity.Auction;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+// Todo: 실제 레포지토리 변경 및 삭제 필요
+public interface TempAuctionRepository extends JpaRepository<Auction, Long> {
+
+    @Query("SELECT a FROM Auction a WHERE a.auctionId = :auctionId AND a.deleted = false")
+    Optional<Auction> findByAuctionIdAndDeletedFalse(Long auctionId);
+}
