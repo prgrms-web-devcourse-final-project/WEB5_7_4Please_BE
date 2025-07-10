f09ec4655393f503fd3fbdaec67c46505a94a114
pbk2312 <pbk2312@inu.ac.kr>
2025-07-11 00:20:17 +0900

    feat(order): 에러코드 추가

--- src/main/java/com/deal4u/fourplease/global/exception/ErrorCode.java
+++ src/main/java/com/deal4u/fourplease/global/exception/ErrorCode.java
@@ -8,7 +8,11 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum ErrorCode {
 
-    ENTITY_NOT_FOUND(HttpStatus.NOT_FOUND, "엔티티를 찾을 수 없습니다.");
+    ENTITY_NOT_FOUND(HttpStatus.NOT_FOUND, "엔티티를 찾을 수 없습니다."),
+    AUCTION_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 경매를 찾을 수 없습니다."),
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 유저를 찾을 수 없습니다."),
+    INVALID_AUCTION_BIDDER(HttpStatus.BAD_REQUEST, "해당 사용자는 경매의 낙찰자가 아닙니다."),
+    INVALID_ORDER_TYPE(HttpStatus.BAD_REQUEST, "유효하지 않은 주문 타입입니다.");
 
     private final HttpStatus status;
     private final String message;
