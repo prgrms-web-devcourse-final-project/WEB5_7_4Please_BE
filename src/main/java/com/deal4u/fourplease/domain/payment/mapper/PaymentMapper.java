23cf5c05579bace0f5edf9580b22eaf06c1859ff
pbk2312 <pbk2312@inu.ac.kr>
2025-07-11 16:44:07 +0900

    feat(payment): Mapper 작성

---
+++ src/main/java/com/deal4u/fourplease/domain/payment/mapper/PaymentMapper.java
@@ -0,0 +1,26 @@
+package com.deal4u.fourplease.domain.payment.mapper;
+
+import com.deal4u.fourplease.domain.order.entity.Order;
+import com.deal4u.fourplease.domain.payment.dto.TossPaymentConfirmRequest;
+import com.deal4u.fourplease.domain.payment.dto.TossPaymentConfirmResponse;
+import com.deal4u.fourplease.domain.payment.entity.Payment;
+import com.deal4u.fourplease.domain.payment.entity.PaymentStatus;
+import java.math.BigDecimal;
+
+public class PaymentMapper {
+
+    private PaymentMapper() {
+        // 인스턴스화 방지
+    }
+
+    public static Payment toPayment(Order order,
+                                    TossPaymentConfirmRequest tossPaymentConfirmRequest,
+                                    TossPaymentConfirmResponse response) {
+        return Payment.builder()
+                .amount(new BigDecimal(tossPaymentConfirmRequest.amount()))
+                .status(PaymentStatus.SUCCESS)
+                .paymentKey(response.paymentKey())
+                .orderId(order.getOrderId())
+                .build();
+    }
+}
