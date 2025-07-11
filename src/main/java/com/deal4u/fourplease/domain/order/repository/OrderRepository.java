2b47f0ce32e763d0494e360d9ba58da7eb769790
pbk2312 <pbk2312@inu.ac.kr>
2025-07-11 16:42:38 +0900

    feat(payment): OrderId로 찾는 로직 작성

--- src/main/java/com/deal4u/fourplease/domain/order/repository/OrderRepository.java
+++ src/main/java/com/deal4u/fourplease/domain/order/repository/OrderRepository.java
@@ -1,7 +1,10 @@
 package com.deal4u.fourplease.domain.order.repository;
 
 import com.deal4u.fourplease.domain.order.entity.Order;
+import com.deal4u.fourplease.domain.order.entity.OrderId;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
+    Optional<Order> findByOrderId(OrderId orderId);
 }
