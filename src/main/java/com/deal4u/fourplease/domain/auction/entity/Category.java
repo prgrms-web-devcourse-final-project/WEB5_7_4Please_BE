d5f53a2573128f24968852be2b1a9525995a095c
pbk2312 <pbk2312@inu.ac.kr>
2025-07-14 14:18:44 +0900

    refactor(order): @GeneratedValue(strategy = GenerationType.IDENTITY) 삭제

--- src/main/java/com/deal4u/fourplease/domain/auction/entity/Category.java
+++ src/main/java/com/deal4u/fourplease/domain/auction/entity/Category.java
@@ -1,8 +1,6 @@
 package com.deal4u.fourplease.domain.auction.entity;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
@@ -14,7 +12,6 @@ import lombok.NoArgsConstructor;
 public class Category {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long categoryId;
     private String name;
 }
