4751877dfd6a56288915f111dba3ec95f27b804e
mipangg <morganjeong119@gmail.com>
2025-07-15 01:20:19 +0900

    feat(Auction-Sale): 판매자 판매내역 조회 controller 기능 구현

--- src/main/java/com/deal4u/fourplease/domain/auction/dto/SellerSaleListResponse.java
+++ src/main/java/com/deal4u/fourplease/domain/auction/dto/SellerSaleListResponse.java
@@ -1,4 +1,16 @@
 package com.deal4u.fourplease.domain.auction.dto;
 
-public record SellerSaleListResponse() {
+import java.math.BigDecimal;
+
+public record SellerSaleListResponse(
+        String thumbnailUrl,
+        String name,
+        BigDecimal maxPrice,
+        BigDecimal startBidPrice,
+        String description,
+        Integer salesCount,
+        Integer bidCount,
+        String status
+) {
+
 }
---
+++ src/main/java/com/deal4u/fourplease/domain/auction/entity/SaleAuctionStatus.java
@@ -0,0 +1,5 @@
+package com.deal4u.fourplease.domain.auction.entity;
+
+public enum SaleAuctionStatus {
+    OPEN, PENDING, SUCCESS, REJECT, INTRANSIT ,DELIVERED, FAIL
+}
