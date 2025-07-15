013986debfa07de57ca25ed4ff0fbee34c6b30e4
hunko <rhwlgns4386@naver.com>
2025-07-16 00:27:30 +0900

    test : 서비스 및 이미지 타입 테스트 작성

---
+++ src/test/java/com/deal4u/fourplease/domain/auction/service/SaveAuctionImageServiceTest.java
@@ -0,0 +1,90 @@
+package com.deal4u.fourplease.domain.auction.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import com.deal4u.fourplease.domain.auction.dto.AuctionImageUrlResponse;
+import com.deal4u.fourplease.domain.auction.factory.AuctionSaveDataFactory;
+import com.deal4u.fourplease.domain.file.service.FileSaver;
+import com.deal4u.fourplease.domain.file.service.SaveData;
+import com.deal4u.fourplease.domain.member.entity.Member;
+import com.deal4u.fourplease.global.exception.GlobalException;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import lombok.Getter;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.multipart.MultipartFile;
+
+class SaveAuctionImageServiceTest {
+
+    @Test
+    @DisplayName("경매 이미지 업로드 요청을 처리한다")
+    void processesAuctionImageUploadRequest() {
+        String path = "/test";
+        String fileName = "test";
+        AuctionSaveDataFactory saveDataFactory = new AuctionSaveDataFactory(mail -> path,
+                () -> fileName);
+
+        FakeFileSaver fakeFileSaver = new FakeFileSaver();
+        SaveAuctionImageService saveAuctionImageService = new SaveAuctionImageService(
+                saveDataFactory,
+                fakeFileSaver);
+
+        MockMultipartFile file = new MockMultipartFile(
+                "file",
+                "file.png",
+                "image/png", // valid MIME type and extension
+                new byte[]{1, 2}
+        );
+        Member test = Member.builder().nickName("test").build();
+
+        AuctionImageUrlResponse upload = saveAuctionImageService.upload(test, file);
+
+        assertThat(new SaveData(path, fileName + ".png")).isEqualTo(
+                fakeFileSaver.getInputSaveData());
+        assertThat(file).isEqualTo(fakeFileSaver.getFile());
+        assertThat(upload.url()).isEqualTo("https://example.com" + path);
+    }
+
+    @Test
+    @DisplayName("이미지가 아니면 업로드 실패")
+    void rejectsUploadWhenFileIsNotValidImage() {
+        AuctionSaveDataFactory saveDataFactory = new AuctionSaveDataFactory(value -> "/test",
+                () -> "test");
+
+        SaveAuctionImageService saveAuctionImageService = new SaveAuctionImageService(
+                saveDataFactory, new FakeFileSaver());
+        MockMultipartFile file = new MockMultipartFile(
+                "file",
+                "file.txt",
+                "text/plain", // valid MIME type and extension
+                new byte[]{1, 2}
+        );
+        Member test = Member.builder().nickName("test").build();
+
+        assertThatThrownBy(() -> saveAuctionImageService.upload(test, file))
+                .isInstanceOf(GlobalException.class);
+    }
+
+    @Getter
+    private static class FakeFileSaver implements FileSaver {
+
+        private SaveData inputSaveData;
+        private MultipartFile file;
+
+        @Override
+        public URL save(SaveData saveData, MultipartFile file) {
+            this.inputSaveData = saveData;
+            this.file = file;
+            try {
+                return URL.of(new URI("https", "example.com", saveData.filePath(), null), null);
+            } catch (URISyntaxException | MalformedURLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
