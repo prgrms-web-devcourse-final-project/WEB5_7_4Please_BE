fc9fa410b8b7c51aeb4c1e8e44977ee11e53c2cb
Yuhan Park <pbk2312@inu.ac.kr>
2025-07-16 12:00:53 +0900

    feat(19) : 결제 부분 검증 로직 추가 및 상태 변경  (#28)
    * chore(order): 주석 삭
    
    * chore(order): 필요없는 메서드 삭제
    
    * feat(order): 업데이트 메서드 추가
    
    * feat(order): 결제 및 Order 부분 수정
    
    * test: 테스트 코드 수정
    
    * refactor(order): 깔끔하게 리팩토링
    
    * refactor(payment): 결제 로직 수정
    
    * chore : 체크 스타일 수
    
    * test: status 상태 수정
    
    * refactor: finally에서 작동하는 문제 해

--- src/main/java/com/deal4u/fourplease/domain/settlement/repository/SettlementRepository.java
+++ src/main/java/com/deal4u/fourplease/domain/settlement/repository/SettlementRepository.java
@@ -1,12 +1,26 @@
 package com.deal4u.fourplease.domain.settlement.repository;
 
 import com.deal4u.fourplease.domain.auction.entity.Auction;
-import com.deal4u.fourplease.domain.bid.entity.Bidder;
 import com.deal4u.fourplease.domain.settlement.entity.Settlement;
 import com.deal4u.fourplease.domain.settlement.entity.SettlementStatus;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface SettlementRepository extends CrudRepository<Settlement, Long> {
-    boolean existsByAuctionAndBidderAndStatus(Auction auction, Bidder bidder,
-                                              SettlementStatus status);
+
+    @Query("SELECT s FROM Settlement s "
+            + "JOIN FETCH s.auction a "
+            + "WHERE a = :auction AND a.status = 'CLOSED'")
+    Optional<Settlement> findByAuctionWithJoin(Auction auction);
+
+    @Query("SELECT s FROM Settlement s "
+            + "JOIN FETCH s.auction "
+            + "WHERE s.status = :status "
+            + "AND s.paymentDeadline < :currentTime")
+    List<Settlement> findExpiredSettlements(@Param("status") SettlementStatus status,
+                                            @Param("currentTime") LocalDateTime currentTime);
 }
