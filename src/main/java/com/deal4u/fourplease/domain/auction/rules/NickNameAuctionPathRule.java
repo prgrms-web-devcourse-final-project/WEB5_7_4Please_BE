4437ab57814a85b980568c6c9f7f0e96904f4a0a
hunko <rhwlgns4386@naver.com>
2025-07-16 15:34:56 +0900

    feat : prefix수정

--- src/main/java/com/deal4u/fourplease/domain/auction/rules/NickNameAuctionPathRule.java
+++ src/main/java/com/deal4u/fourplease/domain/auction/rules/NickNameAuctionPathRule.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class NickNameAuctionPathRule implements AuctionPathRule<String> {
 
-    private static final String PREFIX = "/image";
+    private static final String PREFIX = "image";
 
     @Override
     public String createAuctionPath(String nickName) {
