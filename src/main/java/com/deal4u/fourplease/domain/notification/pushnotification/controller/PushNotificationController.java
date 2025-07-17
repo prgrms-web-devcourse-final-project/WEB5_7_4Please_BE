45a2a65c0b25a5bb13b88e973268f893e467bc01
hunko <rhwlgns4386@naver.com>
2025-07-18 03:42:13 +0900

    feat : 알림 종료 api 작성

---
+++ src/main/java/com/deal4u/fourplease/domain/notification/pushnotification/controller/PushNotificationController.java
@@ -0,0 +1,26 @@
+package com.deal4u.fourplease.domain.notification.pushnotification.controller;
+
+import com.deal4u.fourplease.domain.member.entity.Member;
+import com.deal4u.fourplease.domain.member.repository.MemberRepository;
+import com.deal4u.fourplease.domain.notification.pushnotification.entity.Receiver;
+import com.deal4u.fourplease.domain.notification.pushnotification.service.PushNotificationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/notification")
+@RequiredArgsConstructor
+public class PushNotificationController {
+
+    private final MemberRepository memberRepository;
+    private final PushNotificationService pushNotificationService;
+
+    @PatchMapping("/{notificationId}")
+    void clickNotification(@PathVariable(name = "notificationId") Long id) {
+        Member first = memberRepository.findAll().getFirst();
+        pushNotificationService.click(Receiver.of(first.getMemberId()), id);
+    }
+}
--- src/main/java/com/deal4u/fourplease/domain/notification/pushnotification/service/PushNotificationService.java
+++ src/main/java/com/deal4u/fourplease/domain/notification/pushnotification/service/PushNotificationService.java
@@ -1,11 +1,12 @@
 package com.deal4u.fourplease.domain.notification.pushnotification.service;
 
-import com.deal4u.fourplease.domain.notification.pushnotification.entity.Receiver;
 import com.deal4u.fourplease.domain.notification.pushnotification.entity.PushNotification;
+import com.deal4u.fourplease.domain.notification.pushnotification.entity.Receiver;
 import com.deal4u.fourplease.domain.notification.pushnotification.mapper.PushNotificationMapper;
 import com.deal4u.fourplease.domain.notification.pushnotification.message.PushNotificationMessage;
 import com.deal4u.fourplease.domain.notification.pushnotification.repository.PushNotificationRepository;
 import com.deal4u.fourplease.global.exception.ErrorCode;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -20,6 +21,7 @@ public class PushNotificationService {
         saver.save(PushNotificationMapper.toCreateCommands(pushMessage));
     }
 
+    @Transactional
     public void click(Receiver receiver, Long notificationId) {
         PushNotification pushNotification = pushNotificationRepository.findById(notificationId)
                 .orElseThrow(ErrorCode.ENTITY_NOT_FOUND::toException);
