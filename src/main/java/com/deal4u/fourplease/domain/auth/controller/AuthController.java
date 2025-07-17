70007b13a9355df8b74e30e932b3ee22b81ff523
JJY <jjy38836038@gmail.com>
2025-07-18 00:43:35 +0900

    chore: ci.yml 더미데이터 추가 chore(auth): CustomOauth2UserServiceTest 삭제 refactor(auth): codeStyle 수정 refactor(auth): codeStyle 수정 Merge branch 'dev' into feat/13/JJY refactor(member, auth): member도메인의 logout을 auth도메인으로 이동, 그 밖에 리뷰 반영 feat(member): UpdateMemberRequest DTO 추가 및 회원 수정 요청 적용 test(member, auth): logout member에서 auth로 이동 테스트 수정 refactor(auth): 만료일 하드 코딩 수정 test(auth): 만료일 하드 코딩 수정에 맞게 테스트도 수정 Merge branch 'dev' into feat/13/JJY refactor: codeStyle 수정 Merge branch 'dev' into feat/13/JJY chore: 주석 test(auction): AuctionControllerTests 수git checkout feat/13/JJY정 chore: codeStyle 수정 feat(auth): json응답 방식에서 http헤더 전달 방식으로 수정 feat(auth): 같은 이메일, 다른 플랫폼 충돌 방지 feat(member): 소셜 refreshToken 컬럼 추가 feat(auth): 구글에게 refreshToken 발급 feat(auth): 회원 탈퇴 api 추가

--- src/main/java/com/deal4u/fourplease/domain/auth/controller/AuthController.java
+++ src/main/java/com/deal4u/fourplease/domain/auth/controller/AuthController.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -42,5 +43,13 @@ public class AuthController {
             @AuthenticationPrincipal Member member
     ) {
         return authService.logout(authHeader, member);
+    }
+
+    @DeleteMapping("/members")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public ResponseEntity<Void> disconnectPlatforms(
+            @RequestHeader("Authorization") String authHeader,
+            @AuthenticationPrincipal Member member) {
+        return authService.deactivateMember(authHeader, member);
     }
 }
