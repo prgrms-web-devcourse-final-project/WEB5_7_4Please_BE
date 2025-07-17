ce9037d188b6400f8e5cec5e6f52d6de452d7539
hunko <rhwlgns4386@naver.com>
2025-07-18 03:42:50 +0900

    style : api url수정

--- src/main/java/com/deal4u/fourplease/domain/notification/pushnotification/controller/PushNotificationController.java
+++ src/main/java/com/deal4u/fourplease/domain/notification/pushnotification/controller/PushNotificationController.java
@@ -18,7 +18,7 @@ public class PushNotificationController {
     private final MemberRepository memberRepository;
     private final PushNotificationService pushNotificationService;
 
-    @PatchMapping("/{notificationId}")
+    @PatchMapping("/{notificationId}/clicked")
     void clickNotification(@PathVariable(name = "notificationId") Long id) {
         Member first = memberRepository.findAll().getFirst();
         pushNotificationService.click(Receiver.of(first.getMemberId()), id);
