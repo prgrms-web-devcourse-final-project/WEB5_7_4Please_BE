4c1bd627851fcaee2706ef2ce44bd49a287f6f82
ZELDA31777 <102502882+ZELDA31777@users.noreply.github.com>
2025-07-17 09:18:03 +0900

    feat(29) : Pagenation Common 패키지 처리 (#32)
    * fix : 입찰 도메인의 PageResponse의 Field 수정
    
    * refactor : BaseDateEntity, PageResponse를 common Package로 Refactoring
    
    * refactor : CodeStyle 위배 항목 수정
    
    * refactor : CodeStyle에 위배되지 않도록 수정

--- src/main/java/com/deal4u/fourplease/domain/common/BaseDateEntity.java
+++ src/main/java/com/deal4u/fourplease/domain/common/BaseDateEntity.java
@@ -1,4 +1,4 @@
-package com.deal4u.fourplease.domain;
+package com.deal4u.fourplease.domain.common;
 
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
--- src/main/java/com/deal4u/fourplease/domain/member/entity/Member.java
+++ src/main/java/com/deal4u/fourplease/domain/member/entity/Member.java
@@ -9,6 +9,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,6 +18,8 @@ import org.hibernate.annotations.NaturalId;
 
 @Entity
 @Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member extends BaseDateEntity {
 
