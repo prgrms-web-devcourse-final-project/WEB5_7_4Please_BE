92a5fdf37798bf68d07a5f1bf5ad81995ae3ed99
JJY <jjy38836038@gmail.com>
2025-07-17 20:30:20 +0900

    chore: ci.yml 더미데이터 추가 chore(auth): CustomOauth2UserServiceTest 삭제 refactor(auth): codeStyle 수정 refactor(auth): codeStyle 수정 Merge branch 'dev' into feat/13/JJY refactor(member, auth): member도메인의 logout을 auth도메인으로 이동, 그 밖에 리뷰 반영 feat(member): UpdateMemberRequest DTO 추가 및 회원 수정 요청 적용 test(member, auth): logout member에서 auth로 이동 테스트 수정 refactor(auth): 만료일 하드 코딩 수정 test(auth): 만료일 하드 코딩 수정에 맞게 테스트도 수정 Merge branch 'dev' into feat/13/JJY refactor: codeStyle 수정 Merge branch 'dev' into feat/13/JJY chore: 주석 test(auction): AuctionControllerTests 수정

--- src/test/java/com/deal4u/fourplease/domain/auction/controller/AuctionControllerTests.java
+++ src/test/java/com/deal4u/fourplease/domain/auction/controller/AuctionControllerTests.java
@@ -23,12 +23,14 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
-@WebMvcTest(AuctionController.class)
+@SpringBootTest
+@AutoConfigureMockMvc(addFilters = false)
 class AuctionControllerTests {
 
     @Autowired
