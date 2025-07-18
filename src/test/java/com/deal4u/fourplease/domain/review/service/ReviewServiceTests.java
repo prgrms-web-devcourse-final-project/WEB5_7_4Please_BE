829bb74c2431f9102332eb4d0845d7bb5666924e
ZELDA31777 <3177link@gmail.com>
2025-07-18 11:07:44 +0900

    refactor : PageResponse 경로 수정

--- src/test/java/com/deal4u/fourplease/domain/review/service/ReviewServiceTests.java
+++ src/test/java/com/deal4u/fourplease/domain/review/service/ReviewServiceTests.java
@@ -12,7 +12,7 @@ import com.deal4u.fourplease.domain.auction.entity.AuctionStatus;
 import com.deal4u.fourplease.domain.auction.entity.Product;
 import com.deal4u.fourplease.domain.auction.entity.Seller;
 import com.deal4u.fourplease.domain.auction.repository.AuctionRepository;
-import com.deal4u.fourplease.domain.bid.entity.PageResponse;
+import com.deal4u.fourplease.domain.common.PageResponse;
 import com.deal4u.fourplease.domain.member.entity.Member;
 import com.deal4u.fourplease.domain.member.entity.Role;
 import com.deal4u.fourplease.domain.member.entity.Status;
