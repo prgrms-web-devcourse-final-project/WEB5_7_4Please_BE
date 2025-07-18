1d4c33e8aafdb6d6544b0590074bdecdcd9209bc
mipangg <morganjeong119@gmail.com>
2025-07-18 17:40:27 +0900

    feat(wishlist): wishlist 엔티티 수정

--- src/main/java/com/deal4u/fourplease/domain/wishlist/entity/Wishlist.java
+++ src/main/java/com/deal4u/fourplease/domain/wishlist/entity/Wishlist.java
@@ -1,20 +1,41 @@
 package com.deal4u.fourplease.domain.wishlist.entity;
 
+import com.deal4u.fourplease.domain.auction.entity.Auction;
 import com.deal4u.fourplease.domain.common.BaseDateEntity;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLRestriction;
 
 @Entity
+@Getter
+@SQLRestriction("deleted = false")
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Wishlist extends BaseDateEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long wishlistId;
-    private Long memberId;
-    private Long productId;
+
+    private Long memberId; // member
+
+    @ManyToOne
+    @JoinColumn(name = "auction_id")
+    private Auction auction;
 
     private Boolean deleted = false;
+
+    public void delete() {
+        this.deleted = true;
+    }
 
 }
