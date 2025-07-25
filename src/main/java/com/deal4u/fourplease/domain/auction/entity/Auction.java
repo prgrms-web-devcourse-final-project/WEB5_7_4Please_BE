c68544092fdd60f4983e2b4064235477a91ef891
pbk2312 <pbk2312@inu.ac.kr>
2025-07-25 17:40:26 +0900

    feat: auction status 추

--- src/main/java/com/deal4u/fourplease/domain/auction/entity/Auction.java
+++ src/main/java/com/deal4u/fourplease/domain/auction/entity/Auction.java
@@ -51,15 +51,43 @@ public class Auction extends BaseDateEntity {
 
     private boolean deleted;
 
+    // 경매 종료
     public void close() {
         this.status = AuctionStatus.CLOSED;
     }
 
+    // 패찰 상태로 변경
     public void fail() {
         this.status = AuctionStatus.FAIL;
     }
 
+    // 삭제 처리
     public void delete() {
         this.deleted = true;
+    }
+
+    // 낙찰 상태로 변경
+    public void markAsSuccess() {
+        this.status = AuctionStatus.SUCCESS;
+    }
+
+    // 결제 완료 상태로 변경
+    public void markPaymentCompleted() {
+        this.status = AuctionStatus.PAYMENT_COMPLETED;
+    }
+
+    // 결제 실패 상태로 변경
+    public void markPaymentFailed() {
+        this.status = AuctionStatus.PAYMENT_FAILED;
+    }
+
+    // 배송 중 상태로 변경
+    public void markShipping() {
+        this.status = AuctionStatus.SHIPPING;
+    }
+
+    // 구매 확정 상태로 변경
+    public void markPurchaseConfirmed() {
+        this.status = AuctionStatus.PURCHASE_CONFIRMED;
     }
 }
--- src/main/java/com/deal4u/fourplease/domain/auction/entity/AuctionStatus.java
+++ src/main/java/com/deal4u/fourplease/domain/auction/entity/AuctionStatus.java
@@ -1,5 +1,12 @@
 package com.deal4u.fourplease.domain.auction.entity;
 
 public enum AuctionStatus {
-    OPEN, CLOSED, FAIL
+    OPEN,            // 경매 진행 중
+    SUCCESS,         // 낙찰
+    CLOSED,          // 경매 종료
+    FAIL,            // 패찰
+    PAYMENT_COMPLETED, // 결제 완료
+    PAYMENT_FAILED,   // 결제 실패
+    SHIPPING,        // 배송 중
+    PURCHASE_CONFIRMED, // 구매 확정
 }
