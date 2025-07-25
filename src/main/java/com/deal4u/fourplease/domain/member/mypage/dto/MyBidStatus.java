50d496395cc9877af14979a24860d38b97e0e8b5
ZELDA31777 <3177link@gmail.com>
2025-07-26 01:48:19 +0900

    fix : AuctionStatus 수정으로 인한 InitProcess Value 수정

---
+++ src/main/java/com/deal4u/fourplease/domain/member/mypage/dto/MyBidStatus.java
@@ -0,0 +1,13 @@
+package com.deal4u.fourplease.domain.auction.entity;
+
+// BidStatus
+public enum AuctionStatus {
+    OPEN,            // 경매 진행 중
+    FAIL,            // 유찰
+    CLOSE,           // 경매 종료
+    PENDING,         // 결제 대기
+    SUCCESS,         // 결제 완료
+    REJECTED,        // 차상위 대기
+    INTRANSIT,       // 배송 중
+    DELIVERED       // 구매 확정
+}
---
+++ src/main/java/com/deal4u/fourplease/domain/member/mypage/service/MyPageAuctionHistoryService.java
@@ -0,0 +1,4 @@
+package com.deal4u.fourplease.domain.member.mypage.service;
+
+public class MyPageAuctionHistoryService {
+}
