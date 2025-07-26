4fc60f54f258c71fb146d5df300d566a03401ade
pbk2312 <pbk2312@inu.ac.kr>
2025-07-26 12:36:59 +0900

    feat: 최상위 입찰자 알림 추가

--- src/main/java/com/deal4u/fourplease/domain/settlement/service/HighestBidderNotifier.java
+++ src/main/java/com/deal4u/fourplease/domain/settlement/service/HighestBidderNotifier.java
@@ -4,7 +4,7 @@ import static com.deal4u.fourplease.global.exception.ErrorCode.EMAIL_SEND_FAILED
 import static com.deal4u.fourplease.global.exception.ErrorCode.PUSH_NOTIFICATION_SEND_FAILED;
 
 import com.deal4u.fourplease.domain.auction.entity.Auction;
-import com.deal4u.fourplease.domain.bid.entity.Bid;
+import com.deal4u.fourplease.domain.bid.entity.Bidder;
 import com.deal4u.fourplease.domain.notification.NotificationSender;
 import com.deal4u.fourplease.domain.notification.email.HtmlEmailMessage;
 import com.deal4u.fourplease.domain.notification.pushnotification.message.PushNotificationMessage;
@@ -21,22 +21,21 @@ public class HighestBidderNotifier {
 
     private final NotificationSender notificationSender;
 
-    public void send(Bid highestBid, Auction auction, LocalDateTime paymentDeadline) {
-        sendEmail(highestBid, auction, paymentDeadline);
-        sendPushNotification(highestBid, auction);
+    public void send(Bidder bidder, Auction auction, LocalDateTime paymentDeadline) {
+        sendEmail(bidder, auction, paymentDeadline);
+        sendPushNotification(bidder, auction);
     }
 
-    private void sendEmail(Bid highestBid, Auction auction, LocalDateTime paymentDeadline) {
+    private void sendEmail(Bidder bidder, Auction auction, LocalDateTime paymentDeadline) {
         try {
-            String bidderEmail = highestBid.getBidder().getMember().getEmail();
+            String bidderEmail = bidder.getMember().getEmail();
 
             HtmlEmailMessage emailMessage = HtmlEmailMessage.builder()
                     .templateName("offerHighestBidder")
                     .subject("[Deal4U] 경매 낙찰 안내 및 결제 안내")
                     .addEmail(bidderEmail)
-                    .addData("bidderName", highestBid.getBidder().getMember().getNickName())
+                    .addData("bidderName", bidder.getMember().getNickName())
                     .addData("auctionTitle", auction.getProduct().getName())
-                    .addData("bidAmount", highestBid.getPrice())
                     .addData("paymentDeadline", paymentDeadline)
                     .addData("paymentUrl", "/") // TODO: 내 입찰 내역으로 변경
                     .build();
@@ -48,7 +47,7 @@ public class HighestBidderNotifier {
         }
     }
 
-    private void sendPushNotification(Bid highestBid, Auction auction) {
+    private void sendPushNotification(Bidder bidder, Auction auction) {
         try {
             String body = auction.getProduct().getName() + " 상품에 낙찰되었습니다. 결제 기한을 확인하세요!";
             String url = "/mypage/bid-history"; // TODO: 내 입찰 내역으로 변경
@@ -57,7 +56,7 @@ public class HighestBidderNotifier {
                     .type("HIGHEST_BIDDER_OFFER")
                     .message(body)
                     .url(url)
-                    .addReceiver(highestBid.getBidder().getMember().getMemberId())
+                    .addReceiver(bidder.getMember().getMemberId())
                     .build();
 
             notificationSender.send(pushMessage);
