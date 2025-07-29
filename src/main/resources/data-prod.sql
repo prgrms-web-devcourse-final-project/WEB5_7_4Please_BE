aab3db20015f4529e83ee0a67adae0fa37dc1032
hunko <rhwlgns4386@naver.com>
2025-07-29 11:57:09 +0900

    fix : sql 수정

--- src/main/resources/data-prod.sql
+++ src/main/resources/data-prod.sql
@@ -1,45 +1,71 @@
--- 이 파일은 MySQL 데이터베이스에서 사용됩니다.
+-- 이 파일은 CI 환경의 H2 데이터베이스에서만 사용됩니다.
 
--- 기존 데이터 삭제 (외래키 제약조건을 고려한 순서)
-DELETE FROM bid;
-DELETE FROM auctions;
-DELETE FROM products;
-DELETE FROM product_categories;
-DELETE FROM member;
-DELETE FROM settlement;
-DELETE FROM orders;
+-- 기존 데이터 삭제
+DELETE
+FROM bid;
+DELETE
+FROM auctions;
+DELETE
+FROM products;
+DELETE
+FROM product_categories;
+DELETE
+FROM member;
 
 -- =================================================================
 -- 1. 회원 정보 (Member)
 -- =================================================================
 
 -- 판매자 정보
-INSERT INTO member (email, nick_name, `role`, `status`, provider, created_at, updated_at)
-VALUES ('seller@example.com', '판매자', 'USER', 'ACTIVE', 'naver', NOW(), NOW());
+-- role, status와 같은 예약어는 백틱(`) 또는 큰따옴표(")로 감싸주는 것이 안전합니다.
+INSERT INTO member (email, nick_name, `role`, `status`,provider, created_at, updated_at)
+VALUES ('seller@example.com', '판매자', 'USER', 'ACTIVE','naver' ,CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 
--- 입찰자 정보 (총 21명, ID: 2 ~ 22)
-INSERT INTO member (email, nick_name, `role`, `status`, provider, created_at, updated_at)
-VALUES ('bidderA@example.com', '입찰자A', 'USER', 'ACTIVE', 'naver', NOW(), NOW()), -- ID: 2
-       ('bidderB@example.com', '입찰자B', 'USER', 'ACTIVE', 'naver', NOW(), NOW()), -- ID: 3
-       ('bidderC@example.com', '입찰자C', 'USER', 'ACTIVE', 'naver', NOW(), NOW()), -- ID: 4
-       ('bidderD@example.com', '입찰자D', 'USER', 'ACTIVE', 'naver', NOW(), NOW()), -- ID: 5
-       ('bidderE@example.com', '입찰자E', 'USER', 'ACTIVE', 'naver', NOW(), NOW()), -- ID: 6
-       ('bidderF@example.com', '입찰자F', 'USER', 'ACTIVE', 'naver', NOW(), NOW()), -- ID: 7
-       ('bidderG@example.com', '입찰자G', 'USER', 'ACTIVE', 'naver', NOW(), NOW()), -- ID: 8
-       ('bidderH@example.com', '입찰자H', 'USER', 'ACTIVE', 'naver', NOW(), NOW()), -- ID: 9
-       ('bidderI@example.com', '입찰자I', 'USER', 'ACTIVE', 'naver', NOW(), NOW()), -- ID: 10
-       ('bidderJ@example.com', '입찰자J', 'USER', 'ACTIVE', 'naver', NOW(), NOW()), -- ID: 11
-       ('bidderK@example.com', '입찰자K', 'USER', 'ACTIVE', 'naver', NOW(), NOW()), -- ID: 12
-       ('bidderL@example.com', '입찰자L', 'USER', 'ACTIVE', 'naver', NOW(), NOW()), -- ID: 13
-       ('bidderM@example.com', '입찰자M', 'USER', 'ACTIVE', 'naver', NOW(), NOW()), -- ID: 14
-       ('bidderN@example.com', '입찰자N', 'USER', 'ACTIVE', 'naver', NOW(), NOW()), -- ID: 15
-       ('bidderO@example.com', '입찰자O', 'USER', 'ACTIVE', 'naver', NOW(), NOW()), -- ID: 16
-       ('bidderP@example.com', '입찰자P', 'USER', 'ACTIVE', 'naver', NOW(), NOW()), -- ID: 17
-       ('bidderQ@example.com', '입찰자Q', 'USER', 'ACTIVE', 'naver', NOW(), NOW()), -- ID: 18
-       ('bidderR@example.com', '입찰자R', 'USER', 'ACTIVE', 'naver', NOW(), NOW()), -- ID: 19
-       ('bidderS@example.com', '입찰자S', 'USER', 'ACTIVE', 'naver', NOW(), NOW()), -- ID: 20
-       ('bidder1@example.com', '입찰자1', 'USER', 'ACTIVE', 'naver', NOW(), NOW()), -- ID: 21
-       ('bidder2@example.com', '입찰자2', 'USER', 'ACTIVE', 'naver', NOW(), NOW()); -- ID: 22
+-- 입찰자 정보 (총 20명, ID: 2 ~ 21)
+INSERT INTO member (email, nick_name, role, status,provider ,created_at, updated_at)
+VALUES ('bidderA@example.com', '입찰자A', 'USER', 'ACTIVE','naver', CURRENT_TIMESTAMP,
+        CURRENT_TIMESTAMP), -- ID: 2
+       ('bidderB@example.com', '입찰자B', 'USER', 'ACTIVE','naver', CURRENT_TIMESTAMP,
+        CURRENT_TIMESTAMP), -- ID: 3
+       ('bidderC@example.com', '입찰자C', 'USER', 'ACTIVE','naver', CURRENT_TIMESTAMP,
+        CURRENT_TIMESTAMP), -- ID: 4
+       ('bidderD@example.com', '입찰자D', 'USER', 'ACTIVE','naver', CURRENT_TIMESTAMP,
+        CURRENT_TIMESTAMP), -- ID: 5
+       ('bidderE@example.com', '입찰자E', 'USER', 'ACTIVE','naver', CURRENT_TIMESTAMP,
+        CURRENT_TIMESTAMP), -- ID: 6
+       ('bidderF@example.com', '입찰자F', 'USER', 'ACTIVE','naver', CURRENT_TIMESTAMP,
+        CURRENT_TIMESTAMP), -- ID: 7
+       ('bidderG@example.com', '입찰자G', 'USER', 'ACTIVE','naver', CURRENT_TIMESTAMP,
+        CURRENT_TIMESTAMP), -- ID: 8
+       ('bidderH@example.com', '입찰자H', 'USER', 'ACTIVE','naver', CURRENT_TIMESTAMP,
+        CURRENT_TIMESTAMP), -- ID: 9
+       ('bidderI@example.com', '입찰자I', 'USER', 'ACTIVE','naver', CURRENT_TIMESTAMP,
+        CURRENT_TIMESTAMP), -- ID: 10
+       ('bidderJ@example.com', '입찰자J', 'USER', 'ACTIVE','naver', CURRENT_TIMESTAMP,
+        CURRENT_TIMESTAMP), -- ID: 11
+       ('bidderK@example.com', '입찰자K', 'USER', 'ACTIVE','naver', CURRENT_TIMESTAMP,
+        CURRENT_TIMESTAMP), -- ID: 12
+       ('bidderL@example.com', '입찰자L', 'USER', 'ACTIVE', 'naver',CURRENT_TIMESTAMP,
+        CURRENT_TIMESTAMP), -- ID: 13
+       ('bidderM@example.com', '입찰자M', 'USER', 'ACTIVE','naver', CURRENT_TIMESTAMP,
+        CURRENT_TIMESTAMP), -- ID: 14
+       ('bidderN@example.com', '입찰자N', 'USER', 'ACTIVE','naver', CURRENT_TIMESTAMP,
+        CURRENT_TIMESTAMP), -- ID: 15
+       ('bidderO@example.com', '입찰자O', 'USER', 'ACTIVE', 'naver',CURRENT_TIMESTAMP,
+        CURRENT_TIMESTAMP), -- ID: 16
+       ('bidderP@example.com', '입찰자P', 'USER', 'ACTIVE', 'naver',CURRENT_TIMESTAMP,
+        CURRENT_TIMESTAMP), -- ID: 17
+       ('bidderQ@example.com', '입찰자Q', 'USER', 'ACTIVE', 'naver',CURRENT_TIMESTAMP,
+        CURRENT_TIMESTAMP), -- ID: 18
+       ('bidderR@example.com', '입찰자R', 'USER', 'ACTIVE', 'naver',CURRENT_TIMESTAMP,
+        CURRENT_TIMESTAMP), -- ID: 19
+       ('bidderS@example.com', '입찰자S', 'USER', 'ACTIVE', 'naver',CURRENT_TIMESTAMP,
+        CURRENT_TIMESTAMP), -- ID: 20
+       ('bidder1@example.com', '입찰자1', 'USER', 'ACTIVE','naver', CURRENT_TIMESTAMP,
+        CURRENT_TIMESTAMP), -- ID: 21
+       ('bidder2@example.com', '입찰자2', 'USER', 'ACTIVE', 'naver',CURRENT_TIMESTAMP,
+        CURRENT_TIMESTAMP) -- ID: 22
+;
 
 -- =================================================================
 -- 2. 상품 및 경매 정보 (Product & Auction)
@@ -47,30 +73,42 @@ VALUES ('bidderA@example.com', '입찰자A', 'USER', 'ACTIVE', 'naver', NOW(), N
 
 -- 카테고리 정보
 INSERT INTO product_categories (category_id, name)
-VALUES (0, ''),
-       (1, '패션'),
-       (2, '전자제품'),
-       (3, '스포츠'),
-       (4, '가구'),
-       (5, '생활용품'),
-       (6, '기타');
+VALUES (0, '');
+INSERT INTO product_categories (category_id, name)
+VALUES (1, '패션');
+INSERT INTO product_categories (category_id, name)
+VALUES (2, '전자제품');
+INSERT INTO product_categories (category_id, name)
+VALUES (3, '스포츠');
+INSERT INTO product_categories (category_id, name)
+VALUES (4, '가구');
+INSERT INTO product_categories (category_id, name)
+VALUES (5, '생활용품');
+INSERT INTO product_categories (category_id, name)
+VALUES (6, '기타');
 
 -- 상품 정보
 INSERT INTO products (name, description, thumbnail_url, address, detail_address, zip_code,
                       seller_member_id, category_category_id, phone, deleted,
                       created_at, updated_at)
 VALUES ('최신형 노트북', '한 번도 사용하지 않은 최신형 노트북입니다. 성능이 매우 뛰어납니다.',
-        'https://example.com/images/laptop.jpg', '경기도 평택시 안산로 25번길 21', '100동 102호', '12345', 1, 1,
-        '010-1234-5678', false, NOW(), NOW()),
+        'https://example.com/images/laptop.jpg', '경기도 평택시 안산로 25번길 21', '100동 102호', '12345', 1, 2,
+        '010-1234-5678', false,
+        CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
        ('최신형 IPhone 16', '미개봉 IPhone 16입니다. 256gb, 컬러는 화이트입니다. 쿨거래시 네고 가능합니다.',
         'https://example.com/images/iPhone16.jpg', '경기도 오산시 안산로 25번길 21', '100동 1004호', '12333', 2,
-        1, '010-2222-1111', false, NOW(), NOW()),
-       ('삼성 갤럭시 Z 플립6', '갤럭시 Z 플립6 새제품입니다. 퍼플 컬러이며, 박스 미개봉입니다. 직거래 및 택배 모두 가능입니다.',
+        2,
+        '010-2222-1111', false,
+        CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+       ('삼성 갤럭시 Z 플립6', '갤럭시 Z 플립6 새제품입니다. 퍼플 컬러이며, 박스 미개봉입니다. 직거래 및 택배 모두 가능합니다.',
         'https://example.com/images/galaxyZFlip6.jpg', '서울특별시 마포구 월드컵북로 396', '202호', '04567', 3,
-        1, '010-3333-4444', false, DATE_SUB(NOW(), INTERVAL 4 DAY), DATE_SUB(NOW(), INTERVAL 4 DAY)),
+        2,
+        '010-3333-4444', false,
+        DATEADD('DAY', -4, CURRENT_TIMESTAMP), DATEADD('DAY', -4, CURRENT_TIMESTAMP)),
        ('삭제를 위한 품목', '삭제 테스트를 위해서 추가한 데이터',
-        'https://example.com/images/delete.jpg', '경기도 평택시 안산로 25번길 21', '100동 102호', '12345', 1, 1,
-        '010-1234-5678', false, NOW(), NOW()),
+        'https://example.com/images/delete.jpg', '경기도 평택시 안산로 25번길 21', '100동 102호', '12345', 1, 2,
+        '010-1234-5678', false,
+        CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
        ('애플 아이폰 15 프로 맥스',
         '아이폰 15 프로 맥스 미드나이트 블랙 색상입니다. 박스 개봉만 했으며, 미사용 상태입니다. 직거래만 가능합니다.',
         'https://example.com/images/iphone15promax.jpg',
@@ -78,11 +116,11 @@ VALUES ('최신형 노트북', '한 번도 사용하지 않은 최신형 노트�
         '1501호',
         '61200',
         1,
-        0,
+        1,
         '010-9999-8888',
         false,
-        DATE_SUB(NOW(), INTERVAL 4 DAY),
-        DATE_SUB(NOW(), INTERVAL 4 DAY)
+        DATEADD('DAY', -4, CURRENT_TIMESTAMP),
+        DATEADD('DAY', -4, CURRENT_TIMESTAMP)
        ),
        ('애플 아이폰 1260 프로 맥스',
         '아이폰 1260 프로 맥스 미드나이트 블랙 색상입니다. 박스 개봉만 했으며, 미사용 상태입니다. 직거래만 가능합니다.',
@@ -91,67 +129,92 @@ VALUES ('최신형 노트북', '한 번도 사용하지 않은 최신형 노트�
         '1501호',
         '61200',
         1,
-        0,
+        1,
         '010-9999-8888',
         false,
-        DATE_SUB(NOW(), INTERVAL 4 DAY),
-        DATE_SUB(NOW(), INTERVAL 4 DAY)
-       );
+        DATEADD('DAY', -4, CURRENT_TIMESTAMP),
+        DATEADD('DAY', -4, CURRENT_TIMESTAMP)
+       );;
 
--- 경매 정보
+-- 경메 정보
+-- TIMESTAMPADD를 H2 호환 함수인 DATEADD로 변경하고, status 컬럼을 감쌌습니다.
 INSERT INTO auctions (product_product_id, starting_price, instant_bid_price, start_time,
                       end_time, `status`, deleted, created_at, updated_at)
-VALUES (1, 100000, 800000, NOW(), DATE_ADD(NOW(), INTERVAL 7 DAY), 'OPEN',
-        false, NOW(), NOW()),
-       (2, 1200000, 1500000, NOW(), DATE_ADD(NOW(), INTERVAL 7 DAY), 'OPEN',
-        false, DATE_ADD(NOW(), INTERVAL 1 DAY), DATE_ADD(NOW(), INTERVAL 1 DAY)),
-       (3, 1000, null, DATE_SUB(NOW(), INTERVAL 4 DAY), DATE_SUB(NOW(), INTERVAL 1 DAY), 'CLOSE',
-        false, DATE_ADD(NOW(), INTERVAL 1 DAY), DATE_ADD(NOW(), INTERVAL 1 DAY)),
-       (4, 10000, 800000, NOW(), DATE_ADD(NOW(), INTERVAL 7 DAY), 'OPEN',
-        false, NOW(), NOW()),
-       (5, 1000, null, DATE_SUB(NOW(), INTERVAL 4 DAY), DATE_SUB(NOW(), INTERVAL 1 DAY), 'CLOSE',
-        false, DATE_ADD(NOW(), INTERVAL 1 DAY), DATE_ADD(NOW(), INTERVAL 1 DAY)),
-       (6, 1000, 50000, NOW(), DATE_ADD(NOW(), INTERVAL 7 DAY), 'OPEN',
-        false, NOW(), NOW());
+VALUES (1, 100000, 800000, CURRENT_TIMESTAMP, DATEADD('DAY', 7, CURRENT_TIMESTAMP), 'OPEN',
+        false, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+       (2, 1200000, 1500000, CURRENT_TIMESTAMP, DATEADD('DAY', 7, CURRENT_TIMESTAMP), 'OPEN',
+        false, DATEADD('DAY', 1, CURRENT_TIMESTAMP), DATEADD('DAY', 1, CURRENT_TIMESTAMP)),
+       (3, 1000, null, DATEADD('DAY', -4, CURRENT_TIMESTAMP), DATEADD('DAY', -1, CURRENT_TIMESTAMP), 'CLOSE',
+        false, DATEADD('DAY', 1, CURRENT_TIMESTAMP), DATEADD('DAY', 1, CURRENT_TIMESTAMP)),
+       (4, 10000, 800000, CURRENT_TIMESTAMP, DATEADD('DAY', 7, CURRENT_TIMESTAMP), 'OPEN',
+        false, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+       (5, 1000, null, DATEADD('DAY', -4, CURRENT_TIMESTAMP), DATEADD('DAY', -1, CURRENT_TIMESTAMP), 'CLOSE',
+        false, DATEADD('DAY', 1, CURRENT_TIMESTAMP), DATEADD('DAY', 1, CURRENT_TIMESTAMP)),
+       (6, 1000, 50000, CURRENT_TIMESTAMP, DATEADD('DAY', 7, CURRENT_TIMESTAMP), 'OPEN',
+        false, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+
 
 -- =================================================================
 -- 3. 입찰 정보 (Bid)
 -- =================================================================
--- 총 25개의 입찰 정보. 정렬(가격 내림차순, 시간 오름차순) 테스트를 위해 가격과 시간을 다르게 설정
+-- 총 20(19개 일반 / 1개 삭제)개의 입찰 정보. 정렬(가격 내림차순, 시간 오름차순) 테스트를 위해 가격과 시간을 다르게 설정
+-- Bid 엔티티의 @Embedded Bidder 필드에 맞춰 bidder_member_id, bidder_nick_name 컬럼에 값을 삽입합니다.
 -- 1개는 삭제된 입찰 정보이기 때문에 조회되지 않습니다.
 INSERT INTO bid (auction_id, bidder_member_id, price, bid_time,
                  is_successful_bidder, deleted, created_at, updated_at)
-VALUES (1, 2, 200000, DATE_SUB(NOW(), INTERVAL 20 SECOND), false, false, NOW(), NOW()),
-       (1, 3, 199000, DATE_SUB(NOW(), INTERVAL 19 SECOND), false, false, NOW(), NOW()),
-       (1, 4, 198000, DATE_SUB(NOW(), INTERVAL 18 SECOND), false, false, NOW(), NOW()),
-       (1, 5, 197000, DATE_SUB(NOW(), INTERVAL 17 SECOND), false, false, NOW(), NOW()),
-       (1, 6, 196000, DATE_SUB(NOW(), INTERVAL 16 SECOND), false, false, NOW(), NOW()),
-       (1, 7, 195000, DATE_SUB(NOW(), INTERVAL 15 SECOND), false, false, NOW(), NOW()),
-       (1, 8, 194000, DATE_SUB(NOW(), INTERVAL 14 SECOND), false, false, NOW(), NOW()),
-       (1, 9, 193000, DATE_SUB(NOW(), INTERVAL 13 SECOND), false, false, NOW(), NOW()),
-       (1, 10, 192000, DATE_SUB(NOW(), INTERVAL 12 SECOND), false, false, NOW(), NOW()),
-       (1, 11, 191000, DATE_SUB(NOW(), INTERVAL 11 SECOND), false, false, NOW(), NOW()),
-       (1, 12, 190000, DATE_SUB(NOW(), INTERVAL 10 SECOND), false, false, NOW(), NOW()),
-       (1, 13, 189000, DATE_SUB(NOW(), INTERVAL 9 SECOND), false, false, NOW(), NOW()),
-       (1, 14, 188000, DATE_SUB(NOW(), INTERVAL 8 SECOND), false, false, NOW(), NOW()),
-       (1, 15, 187000, DATE_SUB(NOW(), INTERVAL 7 SECOND), false, false, NOW(), NOW()),
-       (1, 16, 186000, DATE_SUB(NOW(), INTERVAL 6 SECOND), false, false, NOW(), NOW()),
-       (1, 17, 185000, DATE_SUB(NOW(), INTERVAL 5 SECOND), false, false, NOW(), NOW()),
-       (1, 18, 184000, DATE_SUB(NOW(), INTERVAL 4 SECOND), false, false, NOW(), NOW()),
-       (1, 19, 183000, DATE_SUB(NOW(), INTERVAL 3 SECOND), false, false, NOW(), NOW()),
-       (1, 20, 182000, DATE_SUB(NOW(), INTERVAL 2 SECOND), false, true, NOW(), NOW()),
-       (1, 20, 200000, DATE_SUB(NOW(), INTERVAL 1 SECOND), false, false, NOW(), NOW()),
-       (3, 2, 1000, DATE_SUB(NOW(), INTERVAL 1 SECOND), false, false, NOW(), NOW()),
-       (3, 3, 2000, DATE_SUB(NOW(), INTERVAL 1 SECOND), true, false, NOW(), NOW()),
-       (4, 21, 20000, DATE_SUB(NOW(), INTERVAL 1 SECOND), true, false, NOW(), NOW()),
-       (5, 2, 1000, DATE_SUB(NOW(), INTERVAL 1 SECOND), false, false, NOW(), NOW()),
-       (5, 3, 2000, DATE_SUB(NOW(), INTERVAL 1 SECOND), true, false, NOW(), NOW());
+VALUES (1, 2, 200000, TIMESTAMPADD(SECOND, -20, CURRENT_TIMESTAMP), false, false,
+        CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+       (1, 3, 199000, TIMESTAMPADD(SECOND, -19, CURRENT_TIMESTAMP), false, false,
+        CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+       (1, 4, 198000, TIMESTAMPADD(SECOND, -18, CURRENT_TIMESTAMP), false, false,
+        CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+       (1, 5, 197000, TIMESTAMPADD(SECOND, -17, CURRENT_TIMESTAMP), false, false,
+        CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+       (1, 6, 196000, TIMESTAMPADD(SECOND, -16, CURRENT_TIMESTAMP), false, false,
+        CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+       (1, 7, 195000, TIMESTAMPADD(SECOND, -15, CURRENT_TIMESTAMP), false, false,
+        CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+       (1, 8, 194000, TIMESTAMPADD(SECOND, -14, CURRENT_TIMESTAMP), false, false,
+        CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+       (1, 9, 193000, TIMESTAMPADD(SECOND, -13, CURRENT_TIMESTAMP), false, false,
+        CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+       (1, 10, 192000, TIMESTAMPADD(SECOND, -12, CURRENT_TIMESTAMP), false, false,
+        CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+       (1, 11, 191000, TIMESTAMPADD(SECOND, -11, CURRENT_TIMESTAMP), false, false,
+        CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+       (1, 12, 190000, TIMESTAMPADD(SECOND, -10, CURRENT_TIMESTAMP), false, false,
+        CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+       (1, 13, 189000, TIMESTAMPADD(SECOND, -9, CURRENT_TIMESTAMP), false, false,
+        CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+       (1, 14, 188000, TIMESTAMPADD(SECOND, -8, CURRENT_TIMESTAMP), false, false,
+        CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+       (1, 15, 187000, TIMESTAMPADD(SECOND, -7, CURRENT_TIMESTAMP), false, false,
+        CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+       (1, 16, 186000, TIMESTAMPADD(SECOND, -6, CURRENT_TIMESTAMP), false, false,
+        CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+       (1, 17, 185000, TIMESTAMPADD(SECOND, -5, CURRENT_TIMESTAMP), false, false,
+        CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+       (1, 18, 184000, TIMESTAMPADD(SECOND, -4, CURRENT_TIMESTAMP), false, false,
+        CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+       (1, 19, 183000, TIMESTAMPADD(SECOND, -3, CURRENT_TIMESTAMP), false, false,
+        CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+       (1, 20, 182000, TIMESTAMPADD(SECOND, -2, CURRENT_TIMESTAMP), false, true,
+        CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+       (1, 20, 200000, TIMESTAMPADD(SECOND, -1, CURRENT_TIMESTAMP), false, false,
+        CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+       (3, 2, 1000, TIMESTAMPADD(SECOND, -1, CURRENT_TIMESTAMP), false, false,
+        CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+       (3, 3, 2000, TIMESTAMPADD(SECOND, -1, CURRENT_TIMESTAMP), true, false,
+        CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+       (4, 21, 20000, TIMESTAMPADD(SECOND, -1, CURRENT_TIMESTAMP), true, false,
+        CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+       (5, 2, 1000, TIMESTAMPADD(SECOND, -1, CURRENT_TIMESTAMP), false, false,
+        CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+       (5, 3, 2000, TIMESTAMPADD(SECOND, -1, CURRENT_TIMESTAMP), true, false,
+        CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 
--- =================================================================
--- 4. 정산 정보 (Settlement)
--- =================================================================
 INSERT INTO settlement (
-    auction_id,
+    auction_id ,
     bidder_member_id,
     status,
     payment_deadline,
@@ -161,28 +224,25 @@ INSERT INTO settlement (
     updated_at
 ) VALUES (
              3,                     -- auction_id (참조하는 경매 ID)
-             22,                    -- bidder_member_id
+             22,                     -- bidder_id (Bidder 임베디드 필드 안에 들어가는 ID라면 JPA에서는 @Embeddable 처리 필요)
              'PENDING',             -- status (ENUM)
-             DATE_ADD(NOW(), INTERVAL 3 DAY),  -- 결제 마감일: 3일 뒤
+             DATEADD('DAY', 3, CURRENT_TIMESTAMP),  -- 결제 마감일: 3일 뒤
              NULL,                  -- 거절 사유 없음
              NULL,                  -- 결제 완료 시간 없음
-             NOW(),                 -- 생성일
-             NOW()                  -- 수정일
+             CURRENT_TIMESTAMP,     -- 생성일
+             CURRENT_TIMESTAMP      -- 수정일
          ),
          (
              5,                     -- auction_id (참조하는 경매 ID)
-             3,                     -- bidder_member_id
+             3,                     -- bidder_id (Bidder 임베디드 필드 안에 들어가는 ID라면 JPA에서는 @Embeddable 처리 필요)
              'PENDING',             -- status (ENUM)
-             DATE_ADD(NOW(), INTERVAL 3 DAY),  -- 결제 마감일: 3일 뒤
+             DATEADD('DAY', 3, CURRENT_TIMESTAMP),  -- 결제 마감일: 3일 뒤
              NULL,                  -- 거절 사유 없음
              NULL,                  -- 결제 완료 시간 없음
-             NOW(),                 -- 생성일
-             NOW()                  -- 수정일
+             CURRENT_TIMESTAMP,     -- 생성일
+             CURRENT_TIMESTAMP      -- 수정일
          );
 
--- =================================================================
--- 5. 주문 정보 (Orders)
--- =================================================================
 INSERT INTO orders (
     price,
     auction_auction_id,
@@ -201,9 +261,9 @@ INSERT INTO orders (
 ) VALUES (
              2000.00,
              5,
-             NOW(),
+             CURRENT_TIMESTAMP,
              22,
-             NOW(),
+             CURRENT_TIMESTAMP,
              '경기도 성남시 분당구 판교로 235',
              '우림W시티 706호',
              '빠른 배송 부탁드립니다.',
