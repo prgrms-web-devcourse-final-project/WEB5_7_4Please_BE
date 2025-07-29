e7b63e0d4fa3733c598d6af5e22d9aca66ab8216
pbk2312 <pbk2312@inu.ac.kr>
2025-07-29 21:07:55 +0900

    refactor: 누락된 부분 추가

--- src/main/java/com/deal4u/fourplease/domain/bid/repository/BidRepository.java
+++ src/main/java/com/deal4u/fourplease/domain/bid/repository/BidRepository.java
@@ -19,6 +19,7 @@ import java.util.Map;
 import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
